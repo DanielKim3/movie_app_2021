038d6ad9d7d7399c584fa689dc24e201643dae2d
kimwoojin <wjinkim21@naver.com>
2021-08-29 22:46:45 +0900

    add

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ import { thisStringValue } from "es-abstract";
 
 //react는 자동적으로 class component의 render method를 자동으로 실행
 //state는 objec component의 data를 넣을 공간 데이터는 변한다
+//setState를 호출할때마다 react는 새로운 state와 함께 render function을 호출한다.
 
-class App extends React.Component{
-  state = {
-    count: 0
-  };
-  add= () => {
-    this.setState(current =>({ count: current.count + 1}));
-  }; //javascript코드
-  minus= () => {
-    this.setState(current =>({ count: current.count - 1}));
-  }
-  render(){
-    return<div>
-       <h1>The number is: {this.state.count}</h1>
-       <button onClick={this.add}>Add</button>
-       <button onClick={this.minus}>Minus</button>
-    </div>;
+
+class App extends React.Component { 
+ state = {
+   isLoading: true
+ };
+ componentDidMount(){
+   setTimeout(() => {
+    this.setState({ isLoading:false});
+   }, 6000);
+ }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
+    
+  
+  
 
 
 export default App;
